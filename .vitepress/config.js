e233799a38e25e31857de951d388e735c49e7a34
nodoubt0322 <nodoubt0322@gmail.com>
2022-02-19 00:54:58 +0800

    init

---
+++ .vitepress/config.js
@@ -0,0 +1,30 @@
+const { getPosts, generatePaginationPages } = require('./theme/serverUtils')
+
+async function config() {
+    const pageSize = 6
+    await generatePaginationPages(pageSize)
+    return {
+        title: '學習隨記',
+        base:'/',
+        description: 'vitepress,blog',
+        themeConfig: {
+            posts: await getPosts(),
+            pageSize: pageSize,
+            website: 'https://github.com/nodoubt0322/blog',
+            comment: {
+                repo: 'nodoubt0322/blog',
+                themes: 'github-light',
+                issueTerm: 'pathname'
+            },
+            nav: [
+                { text: 'Home', link: '/' },
+                { text: 'Archives', link: '/pages/archives' },
+                { text: 'Tags', link: '/pages/tags' },
+                { text: 'About', link: '/pages/about' }
+            ]
+        },
+        srcExclude: ['README.md']
+    }
+}
+
+module.exports = config()
