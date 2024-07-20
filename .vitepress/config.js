750237ce2c2d08bbb6d72cd6c938966daa5d672e
nodoubt0322 <nodoubt0322@gmail.com>
2024-07-20 09:58:37 +0800

    feat: hide link to first post

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -6,33 +6,30 @@ injectSpeedInsights()
 inject()
 
 async function config() {
-  const pageSize = 10
-  await generatePaginationPages(pageSize)
-  return {
-    title: '學習隨記',
-    base: '/',
-    description: 'vitepress,blog',
-    head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
-    themeConfig: {
-      posts: await getPosts(),
-      pageSize: pageSize,
-      website: 'https://blog-nodoubt0322.vercel.app/',
-      comment: {
-        repo: 'nodoubt0322/blog',
-        issueTerm: 'pathname'
-      },
-      nav: [
-        { text: 'Home', link: '/' },
-        { text: 'Archives', link: '/pages/archives' },
-        { text: 'Tags', link: '/pages/tags' },
-        { text: 'Tools', link: '/pages/tools' },
-        { text: 'Books', link: '/pages/books' },
-        { text: 'ES', link: '/pages/es' },
-        { text: 'About', link: '/pages/about' }
-      ]
-    },
-    srcExclude: ['README.md']
-  }
+    const pageSize = 10
+    await generatePaginationPages(pageSize)
+    return {
+        title: '學習隨記',
+        base: '/',
+        description: 'vitepress,blog',
+        head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+        themeConfig: {
+            posts: await getPosts(),
+            pageSize: pageSize,
+            website: 'https://blog-nodoubt0322.vercel.app/',
+            comment: {
+                repo: 'nodoubt0322/blog',
+                issueTerm: 'pathname'
+            },
+            nav: [
+                { text: 'Home', link: '/' },
+                { text: 'Archives', link: '/pages/archives' },
+                { text: 'Tags', link: '/pages/tags' },
+                { text: 'About', link: '/pages/about' }
+            ]
+        },
+        srcExclude: ['README.md']
+    }
 }
 
 module.exports = config()
